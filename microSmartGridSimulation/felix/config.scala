c6d5b7f764f25fd7601932a794d9e85b825eae6e
sfrey <sfrey@f836604b-0c62-cc57-f957-bc0c4e5c1809>
2013-01-28 17:22:03 +0000

    1.1 naming convention cleared felix cache dropped clerezza shell
    git-svn-id: http://www.infres.enst.fr/svn/mstgrd/trunk@60 f836604b-0c62-cc57-f957-bc0c4e5c1809

--- microSmartGridSimulation/felix/config.scala
+++ microSmartGridSimulation/felix/config.scala
@@ -2,13 +2,16 @@ import java.util.{ List => JList }
 import scala.collection.JavaConversions._
 import fr.tpt.s3.misTiGriD.conf._
 
-:load deploy/house.scala
 
-$[HouseFactory].make(
+
+def deploy() = {
+  $[HouseFactory].make(
       House.atmosphereModel,
       House.aggregator,
       House.walls, House.wallLayouts,
       House.rooms, House.roomLayouts,
       House.heaters, House.heaterLayouts)
+}
+
       
 :load deploy/extra.scala
