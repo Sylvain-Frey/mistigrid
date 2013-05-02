82d84c97c9143e1710c27a2449052cf228b135c4
sfrey <sfrey@f836604b-0c62-cc57-f957-bc0c4e5c1809>
2013-05-02 11:44:49 +0000

    Darkest valley (non-working alba implem X[ )
    git-svn-id: http://www.infres.enst.fr/svn/mstgrd/trunk@114 f836604b-0c62-cc57-f957-bc0c4e5c1809

--- appliances/src/main/java/fr/sylfrey/misTiGriD/appliances/HeaterImpl.java
+++ appliances/src/main/java/fr/sylfrey/misTiGriD/appliances/HeaterImpl.java
@@ -15,14 +15,45 @@ import org.apache.felix.ipojo.annotations.Validate;
 import fr.sylfrey.misTiGriD.electricalGrid.Aggregator;
 import fr.sylfrey.misTiGriD.electricalGrid.BlackOut;
 import fr.sylfrey.misTiGriD.electricalGrid.Prosumer;
+import fr.sylfrey.misTiGriD.environment.Updatable;
 import fr.sylfrey.misTiGriD.temperature.ThermicObject;
 import fr.sylfrey.misTiGriD.temperature.Wall;
 
 @Component(name="Heater",immediate=true)
-@Provides(specifications={Heater.class,Prosumer.class,Wall.class})
-public class HeaterImpl implements Heater {
+@Provides(specifications={Heater.class,Prosumer.class,Wall.class, Updatable.class, ThermicObject.class})
+public class HeaterImpl implements Heater, Updatable {
+
+	/**
+	 * !!
+	 * Added updatable behaviour but period, heatcapacity properties
+	 * are not configurable -> TODO = change deploy bundle, configs, etc. 
+	 * !!
+	 */
+
+	private final int period = 1000;
+	private float temperature;
+	private final float heatCapacity = 100;
+	
+	@Override
+	public void update() {
+		if (getEmissionPower()!=0) { // heater is on
+			temperature = room.getCurrentTemperature() - getProsumedPower()*efficiency;
+		} else { /// heater is off, simulate decay
+			float heatFlux = (room.getCurrentTemperature() - temperature)*
+					period*heatConductance;
+			temperature += heatFlux/heatCapacity;			
+		}
+		// ensure temperature is never lower than room's
+		// htf does that happens??? :/ -> long time between updates??
+		temperature = Math.max(temperature, room.getCurrentTemperature());
+	}
 
 
+	@Override
+	public int getPeriod() {
+		return period;		
+	}
+	
 	@Override
 	public float getEmissionPower() {
 		return -getProsumedPower();
@@ -44,7 +75,8 @@ public class HeaterImpl implements Heater {
 	@Override
 	public float getCurrentTemperature() {
 		//at least the temperature of the enclosing room.
-		return Math.max(-getProsumedPower()*efficiency, room.getCurrentTemperature());
+		//return Math.max(-getProsumedPower()*efficiency, room.getCurrentTemperature());
+		return temperature;
 	}
 
 	@Override
