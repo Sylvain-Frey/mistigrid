9468759b9894447f03014c6fd634f34630ec81fb
sfrey <sfrey@f836604b-0c62-cc57-f957-bc0c4e5c1809>
2013-05-31 16:07:15 +0000

    Refactoring for interns: cleaned up lamp config mess.
    git-svn-id: http://www.infres.enst.fr/svn/mstgrd/trunk@138 f836604b-0c62-cc57-f957-bc0c4e5c1809

--- configs/src/main/scala/fr/sylfrey/misTiGriD/configs/Mouchez.scala
+++ configs/src/main/scala/fr/sylfrey/misTiGriD/configs/Mouchez.scala
@@ -21,6 +21,8 @@ class Config {
   implicit val ec = ExecutionContext.Implicits.global 
     
   @Validate def start() = {
+    
+    // House level definitions: atmosphere, electrical aggregators.
     val atmosphere = "atmosphere"
     val atmosphereModel = Atmosphere(name=atmosphere, isManual=true, temperature=20, minTemperature=12, maxTemperature=22)
 
@@ -37,6 +39,12 @@ class Config {
     val loadManagerURI = "akka://MisTiGriD/user/" + houseLoadManager
     val districtLoadManagerURI = "akka://MisTiGriD@localhost:4004/user/districtLoadManager"
              
+      
+    ////////////////////
+    // IDENTIFICATORS //
+    ////////////////////
+      
+    // Room identificators.
     val kitchen = "kitchen"
     val bathroom = "bathroom"
     val room = "room"
@@ -44,6 +52,12 @@ class Config {
     val entrance = "entrance"
     val wc = "wc"
 
+    // Wall identificators.
+    // Naming convention here: id = initial1 + initial2 + n/s/e/w
+    // (north/south/east/west) where the initials 
+    // designate the separated objects identificators; 
+    // if there is no conflict, the last letter is optional.
+    // Example: akn = Atmosphere Kitchen North.
     val akn = "wall_akn"
     val ab  = "wall_ab"
     val arn = "wall_arn"
@@ -65,14 +79,33 @@ class Config {
     val ke  = "wall_ke"
     val we  = "wall_we"
     
+    // Heater identificators.
     val heaterKitchen = "heater_" + kitchen
     val heaterRoom = "heater_" + room
     val heaterLR1 = "heater_" + livingroom + "_1"
     val heaterLR2 = "heater_" + livingroom + "_2"
     val heaterBathroom = "heater_" + bathroom
     val heaterEntrance = "heater_" + entrance
+        
+    // Lamp identificators.
+    val lamp1 = "lamp_1"
+    val lamp2 = "lamp_2"
+    val lamp3 = "lamp_3"
+    val lamp4 = "lamp_4"
+    val lamp5 = "lamp_5"
 
+      
+      
+      
+      
+    ////////////////////
+    // SPECIFICATIONS //
+    ////////////////////      
+      
+      
+    // Wall specifications.
     val walls = Map[String, Wall](
+      // wallID -> Wall(surfacicHeatConductance, openness, openHeatConductance, length, list of neighbours)
       akn -> Wall(0.01f, false, 0.05f, 3, List(atmosphere, kitchen)),
       ab  -> Wall(0.01f, false, 0.05f, 2, List(atmosphere, bathroom)),
       arn -> Wall(0.01f, false, 0.05f, 4, List(atmosphere, room)),
@@ -94,7 +127,9 @@ class Config {
       ke  -> Wall(0.01f, false, 0.05f, 3, List(kitchen, entrance)),
       we  -> Wall(0.01f, false, 0.05f, 2, List(wc, entrance)))
       
+    // Room specifications.
     val rooms = Map[String, TH](
+      // roomID -> TH(initialTemperature, heatCapacity, list of walls and heaters)
       kitchen    -> TH(24, 12, List(akw, akn, kb, kw, ke, heaterKitchen)),
       bathroom   -> TH(24, 6,  List(ab, br, bw, kb, heaterBathroom)),
       room       -> TH(24, 20, List(arn, rae, rl, er, wr, br, heaterRoom)),
@@ -102,12 +137,15 @@ class Config {
       entrance   -> TH(24, 10, List(ke, we, er, ele, els, ae, heaterEntrance)),
       wc         -> TH(24, 2,  List(bw, wr, we, kw)))
       
+    // Heater (and heater manager) specifications.
     val heaters = Map[String, Tuple2[Heater, HeaterManager]](
+      // heaterID -> ...
       heaterKitchen  -> Tuple2(
+          // Heater(initialProsumedPower, heatConductance, efficiency, maxPower, aggregatorID, roomID)
           Heater(0, 0.02f, 0.1f, 200f, aggregator, kitchen),	
           HeaterManager(
               actorPath = heaterKitchen  + "_manager", 
-              period = 200,
+              period = 400,
               requiredTemperature = 22,
               prosumerStatus = "Flexible",
               houseLoadManagerURI = loadManagerURI,
@@ -118,7 +156,7 @@ class Config {
           Heater(0, 0.02f, 0.1f, 200f, aggregator, room),
           HeaterManager(
               actorPath = heaterRoom     + "_manager", 
-              period = 200,
+              period = 400,
               requiredTemperature = 22,
               prosumerStatus = "Flexible",
               houseLoadManagerURI = loadManagerURI,
@@ -129,7 +167,7 @@ class Config {
           Heater(0, 0.02f, 0.1f, 200f, aggregator, livingroom),
           HeaterManager(
               actorPath = heaterLR1      + "_manager", 
-              period = 200,
+              period = 400,
               requiredTemperature = 22,
               prosumerStatus = "Flexible",
               houseLoadManagerURI = loadManagerURI,
@@ -140,7 +178,7 @@ class Config {
           Heater(0, 0.02f, 0.1f, 200f, aggregator, livingroom),
           HeaterManager(
               actorPath = heaterLR2      + "_manager", 
-              period = 200,
+              period = 400,
               requiredTemperature = 22,
               prosumerStatus = "Flexible",
               houseLoadManagerURI = loadManagerURI,
@@ -151,7 +189,7 @@ class Config {
           Heater(0, 0.02f, 0.1f, 200f, aggregator, bathroom),
           HeaterManager(
               actorPath = heaterBathroom + "_manager", 
-              period = 200,
+              period = 400,
               requiredTemperature = 22,
               prosumerStatus = "Flexible",
               houseLoadManagerURI = loadManagerURI,
@@ -162,7 +200,7 @@ class Config {
           Heater(0, 0.02f, 0.1f, 200f, aggregator, entrance),
           HeaterManager(
               actorPath = heaterEntrance + "_manager", 
-              period = 200,
+              period = 400,
               requiredTemperature = 22,
               prosumerStatus = "Flexible",
               houseLoadManagerURI = loadManagerURI,
@@ -170,14 +208,24 @@ class Config {
               room = entrance,
               kp = 40, ki = 0, kd = 0)))
  
-      
+    // Lamp specifications.
+    val lamps = Map[String, Int](
+      // lampID -> maxPower  
+      lamp1 -> 100,
+      lamp2 -> 100,
+      lamp3 -> 100,
+      lamp4 -> 100,
+      lamp5 -> 100
+    )
       
 
-    ////////////
+    /////////////
     // LAYOUTS //
-    ////////////
+    /////////////
 
+    // Spatial organisation of rooms.
     val roomLayouts = Map[String, Dim](
+      // roomID -> Dim(x, y, width, height, z)
       kitchen    -> Dim(  50,  50, 300, 400, 5),
       bathroom   -> Dim( 350,  50, 200, 300, 5),
       room       -> Dim( 550,  50, 400, 500, 5),
@@ -185,7 +233,9 @@ class Config {
       entrance   -> Dim(  50, 450, 500, 200, 5),
       wc         -> Dim( 350, 350, 200, 100, 5))  
 
+    // Spatial organisation of heaters.
     val heaterLayouts = Map[String, Tuple2[String,Pos]](
+      // heaterID -> (roomID, Pos(x, y, z))
       heaterKitchen 	-> (kitchen, 	Pos(60, 	250, 10)),
       heaterBathroom 	-> (bathroom, 	Pos(360, 	200, 10)),
       heaterRoom 		-> (room, 		Pos(790, 	400, 10)),
@@ -193,7 +243,9 @@ class Config {
       heaterLR2	 		-> (livingroom, Pos(60, 	770, 10)),
       heaterEntrance 	-> (entrance, 	Pos(70, 	550, 10)))
 
+    // Spatial organisation of doors (not intuitive, I know).
     val wallLayouts = Map[String, Dim](
+      // wallID -> Dim(x, y, width, height, z)
       br 	-> Dim(540, 250, 10, 80, 6),
       er 	-> Dim(550, 460, 10, 80, 6),
       els   -> Dim(250, 640, 80, 10, 6),
@@ -201,24 +253,37 @@ class Config {
       ke 	-> Dim(160, 440, 80, 10, 6),
       we 	-> Dim(360, 440, 80, 10, 6))
     
+    // Spatial organisation of lamps.
+    val lampLayouts = Map[String, Tuple2[Int,Int]](
+      // lampID -> (x, y)  
+      lamp1 -> (240, 230),
+      lamp2 -> (580, 330),
+      lamp3 -> (720, 150),
+      lamp4 -> (360, 760),
+      lamp5 -> (610, 760)
+    )
           
+      
     houseFactory.make(
       atmosphereModel,
       aggregatorModel,
       walls, wallLayouts,
       rooms, roomLayouts,
-      heaters, heaterLayouts).foreach( _.future onSuccess {
+      heaters, heaterLayouts,
+      lamps, lampLayouts).foreach( _.future onSuccess {
         case componentInstance => instances += componentInstance
     })
-      
+       
 	
+    // Load manager specification and layout.
+    
     spawn("HouseLoadManagerDeployer",
       "instance.name" -> houseLoadManager,
       "actorPath" -> houseLoadManager,
       "maxConsumption" -> "-1600",
       "hysteresisThreshold" -> "300",
       "prosumerStatus" -> "Flexible",
-      "period" -> "10",
+      "period" -> "50",
       "hasParent" -> "false",
       "districtLoadManagerURI" -> districtLoadManagerURI,
       "requires.from" -> metaFactory.&("aggregator" -> aggregator))
